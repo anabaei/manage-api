17201aae4dea51522c592be7c5664d23c875104e
anabaei <nabaei17@gmail.com>
2023-05-08 22:27:54 -0700

    update swagger urls

--- controllers/shiftController.mjs
+++ controllers/shiftController.mjs
@@ -4,7 +4,7 @@ import shiftService  from '../services/shiftService.mjs';
 export const getAvailableShiftsNotFromFacility = async (req, res) => {
   try {
     const { start_date, end_date, facilityId, page, page_size } = req.query 
-     const shifts = await shiftService.getAllShiftsNotFromFacility(start_date, end_date, facilityId)
+     const shifts = await shiftService.getAllShiftsNotFromFacility(start_date, end_date, facilityId, page, page_size)
    res.json(shifts);
   } catch (err) {
     console.error(err);
@@ -16,7 +16,7 @@ export const getAllShifts = async (req, res) => {
 
   try {
     const { start_date, end_date, page, page_size} = req.query 
-     const shifts = await shiftService.getAvailableShifts(start_date, end_date, page, page_size)
+     const shifts = await shiftService.getAvailableShifts(start_date, end_date, page, page_size, page, page_size)
    res.json(shifts);
   } catch (err) {
     console.error(err);
